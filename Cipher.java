299011006da616839b7f7f5ce89fc796efbcec07
SurajPagad <suraj.pagad61@gmail.com>
2014-06-29 12:38:50 +0530

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -6,28 +6,32 @@ public class Cipher
     {
         flag=false;
     }
-    public int checkpsq(int len)
+    /* Checks whether string length is perfect square and returns the square number or
+    else returns the nearest square number */
+    public int checkperfectsqr(int len)
     {
         int t=0;
-        for(int p=0;p<=len/2;p++) //check if perfect square number
+        for(int p=0;p<=len/2;p++) 
         {
             if((p*p)==len)
             {
                 flag=true;
                 t=p;
                 break;
-            }else if((p*p)<len)//if not a perfect square,calculate nearest perfect square number
+            }else if((p*p)<len)
             {    
                 t=p;
             }
         }
         return t;
     }
+    /* calculates the number of rows or columns to be appended if string length is
+    not a perfect square */
     public int calc(int len,int t)
     {
         int e,s=1,p;
         e=len-(t*t);
-        for(p=1;p<=e;p++)//calculate number of extra rows to be appended at the bottom
+        for(p=1;p<=e;p++)
         {
             if(((p%t)==0)&&(p<e))
             {
@@ -36,7 +40,8 @@ public class Cipher
         }   
         return s+t;
     }
-    public void stringtoarray(String str,int x,int y)//enter the string into 2 dimensional array
+    /* copies the string to a two dimensional array */
+    public void stringtoarray(String str,int x,int y)
     {
         int i,j,k=0;
         b=new char[x][y];
@@ -54,7 +59,8 @@ public class Cipher
             }
         }
     }
-    public void display(int x,int y)//display the elements of array
+    /* Displays the two dimensional array */ 
+    public void display(int x,int y)
     {
         for(int i=0;i<x;i++)
         {
@@ -65,10 +71,11 @@ public class Cipher
         }
         System.out.println();
     }
-    public void encrypt(String str,int len)//encrypts the entered string
+    /* Encrypts the string and displays it */
+    public void encrypt(String str,int len)
     {
         int t,s;
-        t=checkpsq(len);
+        t=checkperfectsqr(len);
         if(flag)
         {
             stringtoarray(str,t,t);
@@ -81,10 +88,11 @@ public class Cipher
             display(t,s);
         }
     }
-    public void decrypt(String str,int len)//decrypts the entered encrypted string
+    /* Encrypts the string and displays it */
+    public void decrypt(String str,int len)
     {
         int t,s;
-        t=checkpsq(len);
+        t=checkperfectsqr(len);
         if(flag)
         {
             stringtoarray(str,t,t);
